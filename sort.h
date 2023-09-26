b80462dc519917bd3c4f15aad48d4a6add3c2fba
Joumana <joumanasalahedin@gmail.com>
2023-09-26 02:27:58 +0200

    task 0

--- sort.h
+++ sort.h
@@ -20,3 +20,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/** TASK FUNCTIONS **/
+
+void bubble_sort(int *array, size_t size);
